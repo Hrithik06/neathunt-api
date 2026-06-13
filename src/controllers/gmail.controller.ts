baad15704cad49c8a112c06d7ab52e2e812e4c96
Hrithik06 <el99el03@gmail.com>
2026-06-13 13:30:49 +0530

    refactor: clean up and comment out unused code in Gmail controller and service

--- src/controllers/gmail.controller.ts
+++ src/controllers/gmail.controller.ts
@@ -1,55 +1,55 @@
-import { Response } from "express";
-import { fetchEmails } from "../services/gmailSync.service.js";
-import { getFullUserById } from "../services/user.service.js";
-import { AuthRequest } from "../types/request.js";
-// import { getUserByGoogleId } from "../services/user.service.js";
-// import { Base64 } from "js-base64";
-// export async function scanGmailForApplications(req: Request, res: Response) {
-//   // TODO: get googleId from req
-//   const googleId = process.env.GOOGLE_ID;
-//   if (!googleId) {
-//     res.status(400).send("Invalid Google Id");
-//     return;
-//   }
-//   const user = await getUserByGoogleId(googleId);
+// import { Response } from "express";
+// import { fetchEmails } from "../services/gmailSync.service.js";
+// import { getFullUserById } from "../services/user.service.js";
+// import { AuthRequest } from "../types/request.js";
+// // import { getUserByGoogleId } from "../services/user.service.js";
+// // import { Base64 } from "js-base64";
+// // export async function scanGmailForApplications(req: Request, res: Response) {
+// //   // TODO: get googleId from req
+// //   const googleId = process.env.GOOGLE_ID;
+// //   if (!googleId) {
+// //     res.status(400).send("Invalid Google Id");
+// //     return;
+// //   }
+// //   const user = await getUserByGoogleId(googleId);
+// //   if (!user) {
+// //     res.status(400).send("Invalid User");
+// //     return;
+// //   }
+
+// //   const { refreshToken, accessToken } = user;
+// //   if (!refreshToken) {
+// //     res.status(500).send("Missing Refresh token of User in DB");
+// //     return;
+// //   }
+// //   if (!accessToken) {
+// //     res.status(500).send("Missing Access token of User in DB");
+// //     return;
+// //   }
+// //   const message = await fetchEmails(refreshToken, accessToken);
+// //   // // res.json(message.data.payload.parts[0].body.data);
+// //   // const encoded = message.data.payload.parts[0].body.data;
+// //   // var bytes = Base64.decode(encoded);
+// //   // // var text = utf8.decode(bytes);
+// //   res.send(message);
+// // }
+
+// export async function scanGmailForApplications(req: AuthRequest, res: Response) {
+//   const userId = req.user.userId;
+
+//   const user = await getFullUserById(userId);
+
 //   if (!user) {
-//     res.status(400).send("Invalid User");
-//     return;
+//     return res.status(400).send("User not found");
 //   }
 
 //   const { refreshToken, accessToken } = user;
-//   if (!refreshToken) {
-//     res.status(500).send("Missing Refresh token of User in DB");
-//     return;
+
+//   if (!refreshToken || !accessToken) {
+//     return res.status(400).send("Gmail not connected");
 //   }
-//   if (!accessToken) {
-//     res.status(500).send("Missing Access token of User in DB");
-//     return;
-//   }
+
 //   const message = await fetchEmails(refreshToken, accessToken);
-//   // // res.json(message.data.payload.parts[0].body.data);
-//   // const encoded = message.data.payload.parts[0].body.data;
-//   // var bytes = Base64.decode(encoded);
-//   // // var text = utf8.decode(bytes);
+
 //   res.send(message);
 // }
-
-export async function scanGmailForApplications(req: AuthRequest, res: Response) {
-  const userId = req.user.userId;
-
-  const user = await getFullUserById(userId);
-
-  if (!user) {
-    return res.status(400).send("User not found");
-  }
-
-  const { refreshToken, accessToken } = user;
-
-  if (!refreshToken || !accessToken) {
-    return res.status(400).send("Gmail not connected");
-  }
-
-  const message = await fetchEmails(refreshToken, accessToken);
-
-  res.send(message);
-}
--- src/services/gmailSync.service.ts
+++ src/services/gmailSync.service.ts
@@ -1,158 +1,158 @@
 
-import { getOAuthClient } from "../config/googleOAuth.js";
-import { gmail } from "@googleapis/gmail";
-import {
-  normalizeMetadataMessage,
-} from "../utils/gmail.utils.js";
-import { GmailClient, GmailMessage } from "../types/gmail.js";
-import { gmailQuery } from "../constants/gmailFilters.js";
-import { getFormattedDate, isDefined, isFulfilled } from "../utils/helper.js";
-import { GaxiosResponse } from "gaxios";
-export async function exchangeCodeForTokens(code: string) {
-  const oauth2Client = getOAuthClient();
-  const { tokens } = await oauth2Client.getToken(code);
-  return tokens;
-}
-
-
-// Use the actual type from googleapis
-// import type { Credentials } from "google-auth-library";
-// export async function getGrantedScopes(tokens: Credentials): Promise<string[]>
-//   return tokens.scope?.split(" ") ?? [];
+// import { getOAuthClient } from "../config/googleOAuth.js";
+// import { gmail } from "@googleapis/gmail";
+// import {
+//   normalizeMetadataMessage,
+// } from "../utils/gmail.utils.js";
+// import { GmailClient, GmailMessage } from "../types/gmail.js";
+// import { gmailQuery } from "../constants/gmailFilters.js";
+// import { getFormattedDate, isDefined, isFulfilled } from "../utils/helper.js";
+// import { GaxiosResponse } from "gaxios";
+// export async function exchangeCodeForTokens(code: string) {
+//   const oauth2Client = getOAuthClient();
+//   const { tokens } = await oauth2Client.getToken(code);
+//   return tokens;
 // }
 
 
-export async function listMessageIds(gmailAPI: GmailClient, maxResults = 500) {
-  try {
-    //get first 10 message ids
-    const today = new Date();
-    const priorDate = new Date(new Date().setDate(today.getDate() - 30));
-
-    const formattedAfterDate = getFormattedDate(priorDate);
-    console.log(today)
-    console.log(priorDate);
-    console.log(formattedAfterDate);
-    const messageListResponse = await gmailAPI.users.messages.list({
-      userId: "me",
-      maxResults: maxResults,
-      // q: gmailQuery.replace(/[\r\n]+/gm, "")
-      // q: `after:${formattedAfterDate}`
-      q: `after:2026/02/07`
-    });
-
-    return messageListResponse;
-  } catch (error) {
-
-    if (error instanceof Error) {
-      console.error("listMessageIds Error:", error.message);
-    } else {
-      console.error("listMessageIds Error:", error);
-    }
-    return null;
-
-  }
-}
-async function fetchMessageById(
-  id: string,
-  gmailAPI: GmailClient,
-  format: "metadata" | "full"
-): Promise<GmailMessage | null> {
-  try {
-    const messageResponse = await gmailAPI.users.messages.get({
-      id,
-      userId: "me",
-      format,
-      metadataHeaders: ["Date", "Subject", "From", "Reply-To"],
-    });
-    return messageResponse.data;
-  } catch (error) {
+// // Use the actual type from googleapis
+// // import type { Credentials } from "google-auth-library";
+// // export async function getGrantedScopes(tokens: Credentials): Promise<string[]>
+// //   return tokens.scope?.split(" ") ?? [];
+// // }
 
 
-    if (error instanceof Error) {
-      console.error(`fetchMessageById (${format}) Error:`, error.message);
-    } else {
-      console.error(`fetchMessageById (${format}) Error:`, error);
-    }
-    return null;
+// export async function listMessageIds(gmailAPI: GmailClient, maxResults = 500) {
+//   try {
+//     //get first 10 message ids
+//     const today = new Date();
+//     const priorDate = new Date(new Date().setDate(today.getDate() - 30));
+
+//     const formattedAfterDate = getFormattedDate(priorDate);
+//     console.log(today)
+//     console.log(priorDate);
+//     console.log(formattedAfterDate);
+//     const messageListResponse = await gmailAPI.users.messages.list({
+//       userId: "me",
+//       maxResults: maxResults,
+//       // q: gmailQuery.replace(/[\r\n]+/gm, "")
+//       // q: `after:${formattedAfterDate}`
+//       q: `after:2026/02/07`
+//     });
+
+//     return messageListResponse;
+//   } catch (error) {
+
+//     if (error instanceof Error) {
+//       console.error("listMessageIds Error:", error.message);
+//     } else {
+//       console.error("listMessageIds Error:", error);
+//     }
+//     return null;
+
+//   }
+// }
+// async function fetchMessageById(
+//   id: string,
+//   gmailAPI: GmailClient,
+//   format: "metadata" | "full"
+// ): Promise<GmailMessage | null> {
+//   try {
+//     const messageResponse = await gmailAPI.users.messages.get({
+//       id,
+//       userId: "me",
+//       format,
+//       metadataHeaders: ["Date", "Subject", "From", "Reply-To"],
+//     });
+//     return messageResponse.data;
+//   } catch (error) {
 
 
-  }
-}
-
-export async function fetchMetadataMessageById(id: string | null | undefined, gmailAPI: GmailClient): Promise<GmailMessage | null> {
-  if (!id) return null;
-
-  return fetchMessageById(id, gmailAPI, "metadata");
-}
-
-export async function fetchFullMessageById(id: string | null | undefined, gmailAPI: GmailClient): Promise<GmailMessage | null> {
-  if (!id) return null;
-  return fetchMessageById(id, gmailAPI, "full");
-}
+//     if (error instanceof Error) {
+//       console.error(`fetchMessageById (${format}) Error:`, error.message);
+//     } else {
+//       console.error(`fetchMessageById (${format}) Error:`, error);
+//     }
+//     return null;
 
 
+//   }
+// }
 
+// export async function fetchMetadataMessageById(id: string | null | undefined, gmailAPI: GmailClient): Promise<GmailMessage | null> {
+//   if (!id) return null;
 
-export async function fetchEmails(refreshToken: string, accessToken: string) {
-  try {
-    const oauth2Client = getOAuthClient();
-    oauth2Client.setCredentials({
-      access_token: accessToken,
-      refresh_token: refreshToken,
-      // Optional: expiry_date: 123456789 (timestamp in ms)
-    });
+//   return fetchMessageById(id, gmailAPI, "metadata");
+// }
 
-    // const gmailAPI = google.gmail({ version: "v1", auth: oauth2Client });
-    const gmailAPI = gmail({ version: "v1", auth: oauth2Client });
-
-    //"??" guard against if messageIds is null/empty (empty inbox)
-    const messageIds = await listMessageIds(gmailAPI).then(
-      (res) => res?.data.messages ?? [],
-    );
-
-
-    //Use allSettled to fetch what you can, skip what fails
-    const results = await Promise.allSettled(
-      messageIds.map((msgRef) => fetchMetadataMessageById(msgRef.id, gmailAPI)),
-    );
+// export async function fetchFullMessageById(id: string | null | undefined, gmailAPI: GmailClient): Promise<GmailMessage | null> {
+//   if (!id) return null;
+//   return fetchMessageById(id, gmailAPI, "full");
+// }
 
 
 
 
-    const rawMessages = results
-      .filter(isFulfilled)
-      .map((result) => result.value)
-      .filter(isDefined);
+// export async function fetchEmails(refreshToken: string, accessToken: string) {
+//   try {
+//     const oauth2Client = getOAuthClient();
+//     oauth2Client.setCredentials({
+//       access_token: accessToken,
+//       refresh_token: refreshToken,
+//       // Optional: expiry_date: 123456789 (timestamp in ms)
+//     });
 
-    // const normalizedMessages = rawMessages.map(normalizeMessage)
-    // const messagesWithBody = normalizedMessages.map(attachBodyPart)
-    // const decodedEmails = messagesWithBody
-    //   .map(decodeEmail)
-    //   .filter(Boolean)
+//     // const gmailAPI = google.gmail({ version: "v1", auth: oauth2Client });
+//     const gmailAPI = gmail({ version: "v1", auth: oauth2Client });
 
-
-
-    const decodedEmails = rawMessages
-      .map(normalizeMetadataMessage)
-
-
-    // const decodedEmails = rawMessages
-    //   .map(normalizeFullMessage)
-    //   .map(attachBodyPart)
-    //   .filter(isDefined)
-    //   .map(decodeEmail)
-    //   .filter(isDefined)
-
-    return decodedEmails
-
-  } catch (error) {
+//     //"??" guard against if messageIds is null/empty (empty inbox)
+//     const messageIds = await listMessageIds(gmailAPI).then(
+//       (res) => res?.data.messages ?? [],
+//     );
 
 
-    if (error instanceof Error) {
-      console.error("fetchEmails Error:", error.message);
-    } else {
-      console.error("fetchEmails Error:", error);
-    }
-    return null;
-  }
-}
+//     //Use allSettled to fetch what you can, skip what fails
+//     const results = await Promise.allSettled(
+//       messageIds.map((msgRef) => fetchMetadataMessageById(msgRef.id, gmailAPI)),
+//     );
+
+
+
+
+//     const rawMessages = results
+//       .filter(isFulfilled)
+//       .map((result) => result.value)
+//       .filter(isDefined);
+
+//     // const normalizedMessages = rawMessages.map(normalizeMessage)
+//     // const messagesWithBody = normalizedMessages.map(attachBodyPart)
+//     // const decodedEmails = messagesWithBody
+//     //   .map(decodeEmail)
+//     //   .filter(Boolean)
+
+
+
+//     const decodedEmails = rawMessages
+//       .map(normalizeMetadataMessage)
+
+
+//     // const decodedEmails = rawMessages
+//     //   .map(normalizeFullMessage)
+//     //   .map(attachBodyPart)
+//     //   .filter(isDefined)
+//     //   .map(decodeEmail)
+//     //   .filter(isDefined)
+
+//     return decodedEmails
+
+//   } catch (error) {
+
+
+//     if (error instanceof Error) {
+//       console.error("fetchEmails Error:", error.message);
+//     } else {
+//       console.error("fetchEmails Error:", error);
+//     }
+//     return null;
+//   }
+// }
